7aa8936824352ea8a8b0fba79fb81784f2992f94
Ethan <Ethan.Jung@obigo.com>
2025-10-21 12:05:54 +0900

    fix: 로그인 타입 임포트 버그 수정

--- src/feature/login/Login.tsx
+++ src/feature/login/Login.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import type { LoginResponseData } from '../type';
+import type { LoginResponseData } from '../../type';
 
 interface LoginPopupProps {
   onClose: () => void;
