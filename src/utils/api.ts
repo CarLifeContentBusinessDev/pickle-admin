713e427aa8d1eecba95558d4badc9f9a4ad648bc
Ethan <Ethan.Jung@obigo.com>
2025-10-27 11:07:14 +0900

    feat: 관리자 토큰 만료시 로그아웃 및 토큰에 따른 로그인 상태 관리

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const api = axios.create({
   baseURL: 'https://pickle.obigo.ai',
@@ -16,3 +17,17 @@ api.interceptors.request.use(
 );
 
 export default api;
+
+
+api.interceptors.response.use(
+  (response) => {
+    const { resultCode } = response.data;
+    if (resultCode === 'E0123') {
+      localStorage.removeItem('accessToken');
+
+      toast.error('로그인 토큰이 만료되었습니다. 다시 로그인해주세요.');
+    }
+
+    return response;
+  }
+);
