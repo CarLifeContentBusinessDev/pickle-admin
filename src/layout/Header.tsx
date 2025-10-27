9d453e522b959d9b152edd025f03082fe658f00f
Ethan <Ethan.Jung@obigo.com>
2025-10-27 11:07:14 +0900

    feat: 관리자 토큰 만료시 로그아웃 및 토큰에 따른 로그인 상태 관리

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getGraphToken } from '../utils/auth';
 import { toast } from 'react-toastify';
 import LoginPopup from '../feature/login/Login';
@@ -7,6 +7,22 @@ import Button from '../components/Button';
 
 const Header = () => {
   const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem('accessToken')
+  );
+  const [loginToken, setLoginToken] = useState(
+    localStorage.getItem('loginToken')
+  );
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setAccessToken(localStorage.getItem('accessToken'));
+      setLoginToken(localStorage.getItem('loginToken'));
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, []);
 
   const handleLogin = async () => {
     if (!localStorage.getItem('accessToken'))
@@ -14,6 +30,7 @@ const Header = () => {
     const tk = await getGraphToken();
     if (tk && localStorage.getItem('accessToken')) {
       localStorage.setItem('loginToken', tk);
+      setLoginToken(tk);
       toast.success('MS 로그인에 성공하였습니다.');
     }
   };
@@ -21,12 +38,14 @@ const Header = () => {
   const handlePopupLoginSuccess = (data: LoginResponseData) => {
     localStorage.setItem('accessToken', data.accessToken);
     localStorage.setItem('refreshToken', data.refreshToken);
+    setAccessToken(data.accessToken);
     toast.success('관리자 로그인에 성공했습니다!');
   };
 
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
+    setAccessToken(null);
     window.location.reload();
   };
 
@@ -38,15 +57,15 @@ const Header = () => {
           PICKLE
         </h1>
         <div className='flex gap-4'>
-          {!localStorage.getItem('accessToken') && (
+          {!accessToken && (
             <Button onClick={() => setShowLoginPopup(true)}>
               관리자 로그인
             </Button>
           )}
-          {localStorage.getItem('accessToken') && (
+          {accessToken && (
             <Button onClick={handleLogout}>관리자 로그아웃</Button>
           )}
-          {!localStorage.getItem('loginToken') && (
+          {!loginToken && (
             <Button onClick={handleLogin}>MS Graph 로그인</Button>
           )}
         </div>
